c51d79b1d39a754117a1485bf497afd67a2a10d5
PRATIK_SANGAMI <pratik9841@outlook.com>
2024-10-01 15:02:46 +0545

    move certs

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,17 @@
 const fs = require("fs");
 const path = require("path");
-const homedir = require("os").homedir();
 
 module.exports = {
   devServer: {
     port: 3000,
     https: {
       key: fs.readFileSync(
-        path.resolve(`${homedir}/.office-addin-dev-certs/localhost.key`)
+        path.resolve(`/.office-addin-dev-certs/localhost.key`)
       ),
       cert: fs.readFileSync(
-        path.resolve(`${homedir}/.office-addin-dev-certs/localhost.crt`)
+        path.resolve(`/.office-addin-dev-certs/localhost.crt`)
       ),
-      ca: fs.readFileSync(
-        path.resolve(`${homedir}/.office-addin-dev-certs/ca.crt`)
-      ),
+      ca: fs.readFileSync(path.resolve(`/.office-addin-dev-certs/ca.crt`)),
     },
   },
 };
